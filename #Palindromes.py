1ddfab28c7ca767ccc78ee34dc80db822c33ecb4
Nishant Sharma <167605116+NishantSharma2004@users.noreply.github.com>
2025-04-08 22:54:19 +0530

    Plaindromes

---
+++ #Palindromes.py
@@ -0,0 +1,110 @@
+#Palindromes
+
+text = input("Enter text: ")
+
+# Remove all spaces...
+text = text.replace(' ','')
+
+# ... and check if the word is equal to reversed itself
+if len(text) > 1 and text.upper() == text[::-1].upper():
+	print("It's a palindrome")
+else:
+	print("It's not a palindrome")
+ 
+#Anagrams
+str_1 = input("Enter the first string: ")
+str_2 = input("Enter the second string: ")
+
+strx_1 = ''.join(sorted(list(str_1.upper().replace(' ',''))))
+strx_2 = ''.join(sorted(list(str_2.upper().replace(' ',''))))
+if len(strx_1) > 0 and strx_1 == strx_2:
+	print("Anagrams")
+else:
+	print("Not anagrams")
+
+#The Digit of Life
+date = input("Enter your birthday date (in the following format: YYYYMMDD or YYYYDDMM, 8 digits): ")
+if len(date) != 8 or not date.isdigit():
+    print("Invalid date format.")
+else:
+    while len(date) > 1:
+        the_sum = 0
+        for dig in date:
+            the_sum += int(dig)
+        print(date)
+        date = str(the_sum)
+    print("Your Digit of Life is: " + date)
+    
+#Find a word!
+word = input("Enter the word you wish to find: ").upper()
+strn = input("Enter the string you wish to search through: ").upper()
+
+found = True
+start = 0
+
+for ch in word:
+	pos = strn.find(ch, start) 
+	if pos < 0:
+		found = False
+		break
+	start = pos + 1
+if found:
+	print("Yes")
+else:
+	print("No")
+
+# Sudoku
+
+# A function that checks whether a list passed as an argument contains
+# nine digits from '1' to '9'.
+def checkset(digs):
+    return sorted(list(digs)) == [chr(x + ord('0')) for x in range(1, 10)]
+
+
+# A list of rows representing the sudoku.
+rows = [ ]
+for r in range(9):
+    ok = False
+    while not ok:
+        row = input("Enter row #" + str(r + 1) + ": ")
+        ok = len(row) == 9 or row.isdigit()
+        if not ok:
+            print("Incorrect row data - 9 digits required")
+    rows.append(row)
+
+ok = True
+
+# Check if all rows are good.
+for r in range(9):
+    if not checkset(rows[r]):
+        ok = False
+        break
+
+# Check if all columns are good.	
+if ok:
+    for c in range(9):
+        col = []
+        for r in range(9):
+            col.append(rows[r][c])
+        if not checkset(col):
+            ok = False
+            break
+
+# Check if all sub-squares (3x3) are good.
+if ok:
+    for r in range(0, 9, 3):
+        for c in range(0, 9, 3):
+            sqr = ''
+            # Make a string containing all digits from a sub-square.
+            for i in range(3):
+                sqr += rows[r+i][c:c+3]
+            if not checkset(list(sqr)):
+                ok = False
+                break
+
+# Print the final verdict.
+if ok:
+    print("Yes")
+else:
+    print("No")
+    
