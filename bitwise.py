d05b07bf6708bab164e988b50576d49382438d4f
Nishant Sharma <167605116+NishantSharma2004@users.noreply.github.com>
2025-06-25 22:50:07 +0530

    bitwise

---
+++ bitwise.py
@@ -0,0 +1,8 @@
+a = 6
+b = 4
+print("using bitwise operator", a & b)  # Bitwise AND
+print("using bitwise operator", a | b)  # Bitwise OR    
+print("using bitwise operator", a ^ b)  # Bitwise XOR
+print("using bitwise operator", ~a)  # Bitwise NOT  
+print("using bitwise operator", a << 2)  # Bitwise left shift
+print("using bitwise operator", a >> 2)  # Bitwise right shift
